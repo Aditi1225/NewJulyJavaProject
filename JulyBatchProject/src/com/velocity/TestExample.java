f5db9317fdfbeb4cfb336d7bbada24b57fe9d585
aditi <aditikul1225@gmail.com>
2024-10-02 17:07:32 +0530

    july batch project added

---
+++ JulyBatchProject/src/com/velocity/TestExample.java
@@ -0,0 +1,10 @@
+package com.velocity;
+
+public class TestExample {
+	public static void main(String[] args) {
+		System.out.println("thius is first git program");
+	System.out.println("hello");
+	}
+	
+
+}
